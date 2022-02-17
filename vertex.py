04da242283d6213ce70d1b7f5471b9ac1910476e
shonig <shonig@lightricks.com>
2022-02-17 17:01:14 +0200

    Allow changing port with arg

--- vertex.py
+++ vertex.py
@@ -1,13 +1,14 @@
+import base64
 import logging
 import os
 import sys
 from pathlib import Path
 import tempfile, shutil
 import joblib
-from fastapi import Request, FastAPI
-from fastapi.responses import FileResponse
 
-import pixray
+from fastapi import Request, FastAPI
+
+
 import uvicorn
 
 READY = False
@@ -16,6 +17,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 # create file handler which logs even debug messages
 fh = logging.FileHandler("spam.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+fh.setFormatter(formatter)
+
 fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 
@@ -32,19 +38,11 @@ def create_temporary_copy(src_path):
     return temp_path
 
 
-#
-# logger.info(f"Downloading artifacts from:{MODEL_ARTIFACTS}")
-# # local_files = download_folder(MODEL_ARTIFACTS)
-# # logger.info(f"Downloading artifacts done. Downloaded files: {local_files}")
-#
-# # model = load_joblib(local_files['model.joblib'])
-# logger.info("model loaded")
-#
-# # user_item_sparse = load_rec_matrix(local_files['user_item_csv.csv'])
-# logger.info("loaded ALS matrix")
-def run(prompt,**kwargs):
+def run(prompt, **kwargs):
+    import pixray
+
     pixray.reset_settings()
-    pixray.add_settings(prompts=prompt, quality="draft", custom_loss="aesthetic",**kwargs)
+    pixray.add_settings(prompts=prompt, quality="draft", custom_loss="aesthetic", **kwargs)
     settings = pixray.apply_settings()
     pixray.do_init(settings)
     run_complete = False
@@ -55,12 +53,13 @@ def run(prompt,**kwargs):
         temp_copy = create_temporary_copy(output_file)
         logger.info(f"output newer version of result {i}")
         i += 1
-    ret_val =  Path(os.path.realpath(temp_copy))
+    ret_val = Path(os.path.realpath(temp_copy))
     return ret_val
 
 
-run("test",iterations=1)
+run("test", iterations=1)
 READY = True
+print(f" READY {READY}")
 
 
 @app.get(AIP_HEALTH_ROUTE)
@@ -73,11 +72,22 @@ def check_health():
 @app.post(AIP_PREDICT_ROUTE)
 async def predict(request: Request):
     body = await request.json()
-    prompts = body["prompt"]
-    ret_val = run(prompts)
-    logger.debug(ret_val)
-    return FileResponse(ret_val, media_type="image/png")
+    prompts = body["instances"]
+    logger.info(f"prompt {prompts}")
+    print(f"prompt {prompts}")
+    output_path = run(prompts, iterations=100)
+    with open(output_path, "rb") as image_data:
+        response = {
+            "predictions": [{"image_bytes": {"b64": base64.b64encode(image_data.read()).decode()}}]
+        }
+    return response
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import argparse
+
+    p = argparse.ArgumentParser()
+    p.add_argument("--host", default="0.0.0.0")
+    p.add_argument("--port", type=int, default=8080)
+    args = p.parse_args()
+    uvicorn.run(app, host=args.host, port=args.port)
